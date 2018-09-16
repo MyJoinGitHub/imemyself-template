84ca7b497db76439c49766bca3d1f8cf57c51bf9
imemyself <linjie_email@163.com>
2018-09-16 17:01:27 +0800

    log4j2日志配置

---
+++ template-domain/src/main/java/com/imemyself/template/domain/BaseDomain.java
@@ -0,0 +1,88 @@
+package com.imemyself.template.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * 模型实体基础类
+ */
+public class BaseDomain implements Serializable {
+
+    /**
+     * 实体标识
+     */
+    private Long id;
+
+    /**
+     * 创建时间
+     */
+    private Date createDate;
+
+    /**
+     * 更新时间
+     */
+    private Date updateDate;
+
+    /**
+     * 创建人
+     */
+    private String creator;
+
+    /**
+     * 更新人
+     */
+    private String updator;
+
+    /**
+     * 备注
+     */
+    private String remark;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public Date getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(Date updateDate) {
+        this.updateDate = updateDate;
+    }
+
+    public String getCreator() {
+        return creator;
+    }
+
+    public void setCreator(String creator) {
+        this.creator = creator;
+    }
+
+    public String getUpdator() {
+        return updator;
+    }
+
+    public void setUpdator(String updator) {
+        this.updator = updator;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+}
